023226ce92f88e83e962d4110fe5cb15d6e4c492
Reece Stevens <reecestevens24@gmail.com>
2021-11-19 22:30:53 -0600

    Add UncheckedSpi variant for high speed write applications

--- tm4c123x-hal/src/spi.rs
+++ tm4c123x-hal/src/spi.rs
@@ -60,6 +60,13 @@ pub struct Spi<SPI, PINS> {
     pins: PINS,
 }
 
+/// SPI peripheral operating in full duplex master mode and which transfers data without checking
+/// status registers. This sacrifices error detection but improves bandwidth.
+pub struct UncheckedSpi<SPI, PINS> {
+    spi: SPI,
+    pins: PINS,
+}
+
 macro_rules! busy_wait {
     ($spi:expr, $flag:ident, $op:ident) => {
         loop {
@@ -265,6 +272,162 @@ macro_rules! hal {
             impl<PINS> crate::hal::blocking::spi::transfer::Default<u16> for Spi<$SPIX, PINS> {}
 
             impl<PINS> crate::hal::blocking::spi::write::Default<u16> for Spi<$SPIX, PINS> {}
+
+            impl<SCK, MISO, MOSI> UncheckedSpi<$SPIX, (SCK, MISO, MOSI)> {
+                /// Configures the SPI peripheral to operate in full duplex master mode
+                pub fn $spiX<F>(
+                    spi: $SPIX,
+                    pins: (SCK, MISO, MOSI),
+                    mode: Mode,
+                    freq: F,
+                    config: &SpiConfig,
+                    clocks: &Clocks,
+                    pc: &sysctl::PowerControl,
+                ) -> Self
+                where
+                    F: Into<Hertz>,
+                    SCK: SckPin<$SPIX>,
+                    MISO: MisoPin<$SPIX>,
+                    MOSI: MosiPin<$SPIX>,
+                {
+                    // power up
+                    sysctl::control_power(
+                        pc, sysctl::Domain::$powerDomain,
+                        sysctl::RunMode::Run, sysctl::PowerState::On);
+                    sysctl::reset(pc, sysctl::Domain::$powerDomain);
+
+                    // write 0 (reset value) for master operation.
+                    spi.cr1.write(|w| w);
+
+                    // SSICC Clock setup
+                    // set to reset value (0 = use system clock)
+                    spi.cc.write(|w| w);
+
+                    // Use Moto/SPI & 8bits data size
+                    let scr: u8;
+                    let mut cpsr = 2u32;
+                    let target_bitrate : u32 = clocks.sysclk.0 / freq.into().0;
+
+                    // Find solution for
+                    // SSInClk = SysClk / (CPSDVSR * (1 + SCR))
+                    // with:
+                    //   CPSDVSR in [2,254]
+                    //   SCR in [0,255]
+
+                    loop {
+                        let scr32 = (target_bitrate / cpsr) - 1;
+                        if scr32 < 255 {
+                            scr = scr32 as u8;
+                            break;
+                        }
+                        cpsr += 2;
+                        assert!(cpsr <= 254);
+                    }
+
+                    let cpsr = cpsr as u8;
+
+                    spi.cpsr.write(|w| unsafe {
+                        w.cpsdvsr().bits(cpsr)
+                    });
+
+                    spi.cr0.modify(|_,w| unsafe {
+                        w.spo().bit(mode.polarity == Polarity::IdleHigh)
+                            .sph().bit(mode.phase == Phase::CaptureOnSecondTransition)
+                            .frf().moto()
+                            .scr().bits(scr)
+                    });
+                    match config.word_size {
+                        SpiWordSize::_8 => spi.cr0.modify(|_, w| w.dss()._8()),
+                        SpiWordSize::_16 => spi.cr0.modify(|_, w| w.dss()._16()),
+                    }
+
+                    // Enable peripheral
+                    spi.cr1.write(|w| w.sse().set_bit());
+
+                    Self { spi, pins }
+                }
+
+                /// Releases the SPI peripheral and associated pins
+                pub fn free(self) -> ($SPIX, (SCK, MISO, MOSI)) {
+                    (self.spi, self.pins)
+                }
+
+                /// Change the clock frequency of the SPI device.
+                pub fn reclock<F>(&mut self, freq: F, clocks: &Clocks) where F: Into<Hertz> {
+                    // Disable peripheral
+                    self.spi.cr1.modify(|_, w| w.sse().clear_bit());
+
+                    let scr: u8;
+                    let mut cpsr = 2u32;
+                    let target_bitrate : u32 = clocks.sysclk.0 / freq.into().0;
+
+                    // Find solution for
+                    // SSInClk = SysClk / (CPSDVSR * (1 + SCR))
+                    // with:
+                    //   CPSDVSR in [2,254]
+                    //   SCR in [0,255]
+
+                    loop {
+                        let scr32 = (target_bitrate / cpsr) - 1;
+                        if scr32 < 255 {
+                            scr = scr32 as u8;
+                            break;
+                        }
+                        cpsr += 2;
+                        assert!(cpsr <= 254);
+                    }
+
+                    let cpsr = cpsr as u8;
+
+                    self.spi.cpsr.write(|w| unsafe { w.cpsdvsr().bits(cpsr) });
+                    self.spi.cr0.modify(|_,w| unsafe { w.scr().bits(scr) });
+
+                    // Enable peripheral again
+                    self.spi.cr1.modify(|_, w| w.sse().set_bit());
+                }
+            }
+
+            impl<PINS> FullDuplex<u8> for UncheckedSpi<$SPIX, PINS> {
+                type Error = Error;
+
+                fn read(&mut self) -> nb::Result<u8, Error> {
+                    // Receive FIFO Not Empty
+                    let r = self.spi.dr.read().data().bits() as u8;
+                    Ok(r)
+                }
+
+                fn send(&mut self, byte: u8) -> nb::Result<(), Error> {
+                    self.spi.dr.write(|w| unsafe {
+                        w.data().bits(byte.into())
+                    });
+                    Ok(())
+                }
+            }
+
+            impl<PINS> FullDuplex<u16> for UncheckedSpi<$SPIX, PINS> {
+                type Error = Error;
+
+                fn read(&mut self) -> nb::Result<u16, Error> {
+                    // Receive FIFO Not Empty
+                    let r = self.spi.dr.read().data().bits() as u16;
+                    Ok(r)
+                }
+
+                fn send(&mut self, byte: u16) -> nb::Result<(), Error> {
+                    self.spi.dr.write(|w| unsafe {
+                        w.data().bits(byte.into())
+                    });
+                    Ok(())
+                }
+            }
+
+            impl<PINS> crate::hal::blocking::spi::transfer::Default<u8> for UncheckedSpi<$SPIX, PINS> {}
+
+            impl<PINS> crate::hal::blocking::spi::write::Default<u8> for UncheckedSpi<$SPIX, PINS> {}
+
+            impl<PINS> crate::hal::blocking::spi::transfer::Default<u16> for UncheckedSpi<$SPIX, PINS> {}
+
+            impl<PINS> crate::hal::blocking::spi::write::Default<u16> for UncheckedSpi<$SPIX, PINS> {}
         )+
     }
 }
